a14b25b4118d4fb9f6b0b970c1ffde288a0ca986
vanshdhameliya <vanshdhameliya07@gmail.com>
2025-04-04 10:37:34 +0530

    Reactjs

--- lec-7/src/App.jsx
+++ lec-7/src/App.jsx
@@ -1,78 +1,50 @@
-import { useEffect, useState } from 'react'
-import './index.css'
+import { useEffect, useState } from 'react';
+import './index.css';
 
 function App() {
+  const [getrecipes, setGetrecipe] = useState([]);
 
-  let [getrecipes, setGetrecipe] = useState([])
-
-
-  let getrecipe = () => {
+  const getrecipe = () => {
     fetch('https://dummyjson.com/recipes')
       .then(res => res.json())
-      .then((data) => {
+      .then(data => {
         setGetrecipe(data.recipes);
-
-      })
-  }
+      });
+  };
 
   useEffect(() => {
     setTimeout(() => {
-      getrecipe()
-    })
-  }, [])
+      getrecipe();
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-
+    <div className="container py-4">
+      <h2 className="text-center mb-4">Recipe Cards</h2>
+      <div className="row">
+        {getrecipes.map(recipe => (
+          <div key={recipe.id} className="col-md-4 mb-4">
+            <div className="card h-100 shadow-sm">
+              <img
+                src={recipe.image}
+                className="card-img-top"
+                alt={recipe.name}
+                style={{ objectFit: 'cover' }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{recipe.name}</h5>
+                <p><strong>Meal Type:</strong> {recipe.mealType.join(', ')}</p>
+                <p><strong>Rating:</strong> {recipe.rating}</p>
+                <p><strong>Tags:</strong> {recipe.tags.join(', ')}</p>
+                <p><strong>Ingredients:</strong><br />{recipe.ingredients.join(', ')}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-
-
-      <table align="center" border={1} cellPadding={5} cellSpacing={1}>
-        <thead>
-          <tr>
-            <td>id</td>
-            <td>name</td>
-            <td>image</td>
-            <td>ingredients</td>
-            <td>mealType
-            </td>
-            <td>rating
-            </td>
-            <td>tags</td>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            getrecipes.map((val, i) => {
-              return (
-                <tr key={val.id}>
-                  <td>{val.id}</td>
-                  <td>{val.name}</td>
-                  <td>
-                    <img src={val.image} width="100" alt="" />
-                  </td>
-                  <td>
-                    {val.ingredients.join(" - ")}
-                  </td>
-                  <td>
-                    {val.mealType
-                    }
-                  </td>
-                  <td>{val.rating}</td>
-                  <td>
-                    {val.tags.join("-")}
-                  </td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
-
-
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+
+export default App;
