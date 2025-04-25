63c870562fc98e7c260b56da8d2637a3898da861
vanshdhameliya <vanshdhameliya07@gmail.com>
2025-04-25 18:47:34 +0530

    Reactjs

--- Pr 2. Counter/src/Counter.jsx
+++ Pr 2. Counter/src/Counter.jsx
@@ -4,8 +4,8 @@ import './Counter.css'; // Import the CSS file
 const Counter = ({ cnt, plus, minus, reset }) => {
   return (
     <div className="counter-container">
-      <h1 className="counter-title">Counter</h1>
-      <h2 className="counter-value">Count: {cnt}</h2>
+      <h1 className="counter-title">Counter App</h1>
+      <h2 className="counter-value">Count : {cnt}</h2>
       <div className="button-group">
         <button className="btn plus" onClick={plus}>+</button>
         <button className="btn minus" disabled={cnt === 0} onClick={minus}>-</button>
