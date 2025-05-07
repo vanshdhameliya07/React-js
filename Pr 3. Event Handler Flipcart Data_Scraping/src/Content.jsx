351c3f6d0db87581a32839021081adf6687724c0
vanshdhameliya <vanshdhameliya07@gmail.com>
2025-05-07 13:35:14 +0530

    Reactjs

--- Pr 3. Event Handler Flipcart Data_Scraping/src/Content.jsx
+++ Pr 3. Event Handler Flipcart Data_Scraping/src/Content.jsx
@@ -11,27 +11,28 @@ const Content = ({ content }) => {
                 content.map((v, i) => {
                     return (
                         <div>
-                            <h6 style={{ color: "gray",fontSize:"18px" }}>{v.title}</h6>
+                            <h6 >{v.title}</h6>
                             <p>{v.content2}</p>
                             <p>{v.content3}</p>
-                            <h6 style={{ color: "gray",fontSize:"18px" }}>{v.title2}</h6>
+                            <h6 c>{v.title2}</h6>
                             <p>{v.content4}</p>
-                           
-                          
-                           <p>{v.content5}</p>
+
+
+                            <p>{v.content5}</p>
                             <p>{v.content6}</p>
                             <p>{v.content7}</p>
                             <p>{v.content8}</p>
                             <p>{v.content9}</p>
                             <p>{v.content10}</p>
-                           
-                            <h6 style={{ color: "gray",fontSize:"18px" }}>{v.title3}</h6>
+
+                            <h6 >{v.title3}</h6>
                             <p>{v.content11}</p>
 
-                            <h6 style={{ color: "gray",fontSize:"18px" }}>{v.title4}</h6>
+                            <h6 >{v.title4}</h6>
                             <p>{v.content12}</p>
 
-
+                            <h6 >{v.title5}</h6>
+                            <p>{v.content13}</p>
 
 
                         </div>
