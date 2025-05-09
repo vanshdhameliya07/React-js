3a253fd4b3c51849c3fdf6e9142a2ba28a1d0f1d
vanshdhameliya <vanshdhameliya07@gmail.com>
2025-05-09 10:48:57 +0530

    Reactjs

---
+++ lec-18/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react"
+
+function App() {
+
+  let [input, setInput] = useState({
+    name: ''
+  })
+
+  let change = (e) => {
+
+    const { name, value } = e.target;
+    console.log(name, value);
+
+    const obj = {
+      ...input,
+      [name]: value
+    }
+
+    setInput(obj);
+
+  }
+
+  let submit = (e) => {
+    e.preventDefault()
+
+  }
+
+  return (
+    <>
+      <div align="center ">
+        <form onSubmit={submit} >
+          name:-
+          <input type="text" onChange={change} name="name" placeholder="enter your name" value={input.username} />
+          <br />
+          <button type="submit" >submit</button>
+        </form>
+
+        <table>
+          <thead>
+            <tr>
+              <td>name</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              obj.map((v, i) => {
+                console.log(v);
+
+              })
+            }</tbody>
+
+        </table>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
