d46e7bad8ef7440f6ecbb6f3ae8b50a3b346028a
vanshdhameliya <vanshdhameliya07@gmail.com>
2025-05-09 18:39:46 +0530

    Reactjs

--- Pr 3. Event Handler Flipcart Data_Scraping/src/Footer.jsx
+++ Pr 3. Event Handler Flipcart Data_Scraping/src/Footer.jsx
@@ -14,12 +14,13 @@ const Footer = ({ footer }) => {
 
             <div className="footer-top">
                 <div class="footer-column">
-                    <h4>ABOUT</h4>
+
 
                     {
                         footer.map((v, i) => {
                             return (
                                 <div >
+                                    <h4>{v.ABOUT}</h4>
                                     <a href="">
                                         {v.aboutus}
                                     </a>
@@ -31,12 +32,13 @@ const Footer = ({ footer }) => {
 
                 </div>
                 <div class="footer-column">
-                    <h4>GROUP COMPANIES</h4>
 
                     {
                         footer.map((v, i) => {
                             return (
                                 <div >
+                                    <h4>{v.s}</h4>
+
                                     <a href="">
                                         {v.GROUPCOMPANIES}
                                     </a>
@@ -48,12 +50,13 @@ const Footer = ({ footer }) => {
 
                 </div>
                 <div class="footer-column">
-                    <h4>HELP</h4>
 
                     {
                         footer.map((v, i) => {
                             return (
                                 <div >
+                                    <h4>{v.h}</h4>
+
                                     <a href="">
                                         {v.HELP}
                                     </a>
@@ -64,12 +67,13 @@ const Footer = ({ footer }) => {
 
                 </div>
                 <div class="footer-column">
-                    <h4>CONSUMER POLICY</h4>
 
                     {
                         footer.map((v, i) => {
                             return (
                                 <div >
+                                    <h4>{v.c}</h4>
+
                                     <a href="">
                                         {v.CONSUMERPOLICY}
                                     </a>
@@ -80,38 +84,27 @@ const Footer = ({ footer }) => {
 
                 </div>
                 <div class="footer-column address">
-                    <h4>Mail Us:</h4>
 
                     {
                         footer.map((v, i) => {
                             return (
                                 <div key={i}>
+                                    <h4>{v.m}</h4>
+
                                     <p>{v.MailUs}</p>
                                 </div>
                             );
                         })
                     }
 
-
-                    <h4>Social:</h4>
-                    <div class="social-icons">
-                        {/* {
-                            footer.map((v, i) => {
-                                return (
-                                    <div >
-                                        <img src={v.img} alt="" />
-                                    </div>
-                                )
-                            })
-                        } */}
-                    </div>
                 </div>
                 <div class="footer-column address">
-                    <h4>Registered Office Address:</h4>
                     {
                         footer.map((v, i) => {
                             return (
                                 <div >
+                                    <h4>{v.r}</h4>
+
                                     <p>{v.RegisteredOfficeAddress}</p>
                                 </div>
                             )
@@ -120,25 +113,43 @@ const Footer = ({ footer }) => {
                 </div>
             </div>
 
-            <div class="footer-bottom">
+            <div class=" footer-bottom">
                 <div>
                     {
                         footer.map((v, i) => {
                             return (
-                                <div >
-                                    <img src={v.img} alt="" />
-                                    <p> {v.BecomeaSeller}</p>
+                                <div className='d-flex  jusify-content-center' >
+                                    <img style={{ marginLeft: "30px" }} src={v.seller} alt="" />
+                                    <p style={{ margin: "0px", marginLeft: "5px" }}>{v.b}</p>
+                                    <img style={{ marginLeft: "30px" }} src={v.ad} alt="" />
+                                    <p style={{ margin: "0px", marginLeft: "5px" }}>{v.a}</p>
+
+                                    <img style={{ marginLeft: "30px" }} src={v.g} alt="" />
+                                    <p style={{ margin: "0px", marginLeft: "5px" }}>{v.gi}</p>
+
+                                    <img style={{ marginLeft: "30px" }} src={v.he} alt="" />
+                                    <p style={{ margin: "0px", marginLeft: "5px" }}>{v.hel}</p>
+
                                 </div>
                             )
                         })
                     }
                 </div>
-                <div>&copy; 2007-2025 Flipkart.com</div>
+                <div>{
+                    footer.map((v, i) => {
+                        return (
+                            <div >
+                                <p style={{marginTop:"10px"}}> {v.rr}</p>
+                            </div>
+                        )
+                    })
+                }</div>
                 <div className="payment-icons">
                     {
                         footer.map((v, i) => {
+
                             return (
-                                <img src={v.img.img} alt="" />
+                                <img src={v.img} alt="" />
                             )
                         })
                     }
