de246cadaa807237a975479e979e8768e4365dc2
vanshdhameliya <vanshdhameliya07@gmail.com>
2025-05-10 10:07:02 +0530

    Reactjs

--- Pr 3. Event Handler Flipcart Data_Scraping/src/Banner2.jsx
+++ Pr 3. Event Handler Flipcart Data_Scraping/src/Banner2.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import './Banner2.css'
 
-const Banner2 = ({banner2}) => {
-    console.log(banner2);
-    
+const Banner2 = ({ banner2 }) => {
+
     return (
         <div className='mt-3'>
 
             {
-                banner2.map((v,i)=>{
-                    return(
-                        <img style={{width:"1504px", margin:"auto",display:"block",cursor:"pointer"}} src={v.img} alt="" srcset="" />
+                banner2.map((v, i) => {
+                    return (
+                        <div key={i}>
+                            <img style={{ width: "1504px", margin: "auto", display: "block", cursor: "pointer" }} src={v.img} alt="" />
+                        </div>
                     )
                 })
             }
+            <div className='img-2 mt-3 d-flex'>
+                {
+                    banner2.map((v, i) => {
+                        return (
+                            <img src={v.img2} />
+                           
+                        )
+                    })
+                }
+            </div>
+
+
 
         </div>
     )
