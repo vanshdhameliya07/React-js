f27df835d564af8a478a7747f76904121b5e2bef
vanshdhameliya <vanshdhameliya07@gmail.com>
2025-05-22 09:47:35 +0530

    Reactjs

--- pr-5/src/Banner.jsx
+++ pr-5/src/Banner.jsx
@@ -2,21 +2,20 @@ import React from 'react'
 import './Banner.css'
 const Banner = () => {
     return (
-        <div className='contaner'>
+        <div className='contaner banner'>
 
-            <div className="col-12 d-flex">
-                <div className="col-6">
-                    <span>100% Organic Fruits</span>
-                    <h1>Explore fresh &
-                        <br />
-                        juicy fruits.</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reiciendis
-                        beatae consequuntur.</p>
-                    <button>shop now</button>
-                </div>
-                <div className="col-6">
-                    <img src="https://i.pinimg.com/236x/38/08/f8/3808f8b6dd35d55dbb84e93285e8d2eb.jpg" alt="" />
-                </div>
+            <div className="col-12 banner-content">
+
+                <h2><span>100%</span> Organic Fruits</h2>
+
+                <h1>Explore fresh &
+                    <br />
+                    juicy fruits.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reiciendis
+                    beatae consequuntur.</p>
+                    
+                <button>shop now</button>
+
 
             </div>
 
