2b6879ef2fe63b494cd5bef83636ad6ae921f281
vanshdhameliya <vanshdhameliya07@gmail.com>
2025-05-25 07:39:55 +0530

    Reactjs

--- pr 5. Integrate Bootstrap template/src/Banner.jsx
+++ pr 5. Integrate Bootstrap template/src/Banner.jsx
@@ -4,22 +4,29 @@ const Banner = () => {
     return (
         <div className='contaner banner'>
 
-            <div className="col-12 banner-content">
+            <div className="col-12 d-flex banner-content align-items-center">
 
-                <h2><span>100%</span> Organic Fruits</h2>
+                <div className="col-6">
 
-                <h1>Explore fresh &
-                    <br />
-                    juicy fruits.</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-                    <br />
+                    <h2><span>100%</span> Organic Fruits</h2>
 
-                    reiciendis
-                    beatae consequuntur.</p>
+                    <h1>Explore fresh &
+                        <br />
+                        juicy fruits.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+                        <br />
 
-               <div className='btn'>
-                <a href="">shop now</a>
-               </div>
+                        reiciendis
+                        beatae consequuntur.</p>
+
+                    <div className='btn'>
+                        <a href="">shop now</a>
+                    </div>
+                </div>
+
+                <div className="bg col-6">
+                    <img src="./src/assets/img.png" alt="" />
+                </div>
 
 
             </div>
